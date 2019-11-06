c7f19cbc95c7c99801e753ddb5457694ade073ff
Matheus Rocha desktop <theusrsilva@gmail.com>
2019-11-05 22:08:02 -0300

    ajustando uns paranaue 2

--- view/home.php
+++ view/home.php
@@ -10,24 +10,27 @@
             <label>
                 <input type="radio" name="pokemon" value="Pokémon 1"/> 
                 <img class="pkm" src="assets/pokemons/612.gif" alt="">
+                <p class="pkm-name">Bulbassaur</p>
             </label>
           </div>  
           <div class="col-xl-4 text-center ">
             <label>
                 <input type="radio" name="pokemon" value="Pokémon 2"/> 
                 <img class="pkm" src="assets/pokemons/348.gif" alt="">
-            </label>
-          </div>  
-          <div class="col-xl-4 text-center ">
-            <label>
+                <p class="pkm-name">Bulbassaur</p>
+              </label>
+            </div>  
+            <div class="col-xl-4 text-center ">
+              <label>
                 <input type="radio" name="pokemon" value="Pokémon 3"/> 
                 <img class="pkm" src="assets/pokemons/245.gif" alt="">
-            </label>
-          </div>   
-        </div>
-      <div class="row"> 
-        <div class="col-xl-12 text-center button" id="submit">
-          <button type="submit" name="submit" value="Submit">Vote aqui!</button>
-        </div>
-      </div>
-    </form>
+                <p class="pkm-name">Bulbassaur</p>
+              </label>
+            </div>   
+          </div>
+          <div class="row"> 
+            <div class="col-xl-12 text-center button" id="submit">
+              <button type="submit" name="submit" value="Submit">Vote aqui!</button>
+            </div>
+          </div>
+        </form>
